04982b7d635f3e396f04c63b2090a3bbe7866a1d
Tyler Marrs <tylerwmarrs@gmail.com>
2020-03-07 21:41:07 -0600

    add custom theme based on bootstrap 4 and spacelab bootswatch

--- website/conf.py
+++ website/conf.py
@@ -153,7 +153,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootblog4"
+THEME = "bs4_spacelab"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
