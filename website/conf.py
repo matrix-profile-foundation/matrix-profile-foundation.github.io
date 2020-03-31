2e9f582846a7ddc3b52337462d5e891847905ef6
Tyler Marrs <tylerwmarrs@gmail.com>
2020-03-30 21:23:12 -0500

    fix github deploy branch in conf.py

--- website/conf.py
+++ website/conf.py
@@ -659,10 +659,10 @@ REDIRECTIONS = []
 # https://getnikola.com/handbook.html#deploying-to-github
 # You will need to configure the deployment branch on GitHub.
 GITHUB_SOURCE_BRANCH = 'src'
-GITHUB_DEPLOY_BRANCH = 'master'
+GITHUB_DEPLOY_BRANCH = 'gh-pages'
 
 # The name of the remote where you wish to push to, using github_deploy.
-GITHUB_REMOTE_NAME = 'gh-pages'
+GITHUB_REMOTE_NAME = 'origin'
 
 # Whether or not github_deploy should commit to the source branch automatically
 # before deploying.
